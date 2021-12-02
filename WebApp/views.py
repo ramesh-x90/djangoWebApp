fa765f471d41773f3bc5f0c9639f054c761f7e4b
RAMESH <rameshshyaman@gmail.com>
2021-12-02 23:55:37 +0530

    additional validations

--- WebApp/views.py
+++ WebApp/views.py
@@ -35,7 +35,7 @@ class PatientResister(APIView):
 
                 email = serialized_OBJ.validated_data.get('Email')
                 if Patient.objects.filter(Email=email).exists():
-                    return Response({'error': 'email already exists'}, status=status.HTTP_400_BAD_REQUEST)
+                    raise('email already exists')
                 serialized_OBJ.save()
                 data["message"] = "Patient registered successfully"
                 return Response(data, status=status.HTTP_200_OK)
