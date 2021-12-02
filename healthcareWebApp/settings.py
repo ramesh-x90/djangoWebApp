45de7525293049a63c19d09e0107e795f93dd98b
RAMESH <rameshshyaman@gmail.com>
2021-12-02 15:10:09 +0530

    security fix

--- healthcareWebApp/settings.py
+++ healthcareWebApp/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^*1ck(d&)jap^#rrk)vn4puejgc44@__hq*nogjijvtw#o@sln'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS=['*']
 ALLOWED_HOSTS=['https://healthcarewebapp.herokuapp.com']
@@ -132,8 +132,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+SECURE_HSTS_SECONDS = 31536000 
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 
-CORS_ORIGIN_ALLOW_ALL = True
+
+# CORS_ORIGIN_ALLOW_ALL = True
 
 
 
